deaeea6b8ce94f461ad1c231f7c703c25fb25e3c
guptariya-codes <guptariyaa0412@gmail.com>
2026-07-05 19:21:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,41 @@ scrollTopBtn.addEventListener("click", function () {
     });
 
 });
+// menu btn
+const menuBtn = document.querySelector("#menu-btn");
+const navLinks = document.querySelector("#nav-links");
+
+menuBtn.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+});
+const links = document.querySelectorAll("#nav-links a");
+
+links.forEach(link => {
+    link.addEventListener("click", function () {
+        navLinks.classList.remove("active");
+    });
+});
+
+// Add to Cart Notification
+
+const cartButtons = document.querySelectorAll(".product-card button");
+const notification = document.querySelector("#cart-notification");
+
+cartButtons.forEach(button => {
+
+    button.addEventListener("click", function () {
+
+        const productName =
+            this.parentElement.querySelector("h3").textContent;
+
+        notification.textContent = `✅ ${productName} added to cart!`;
+
+        notification.classList.add("show");
+
+        setTimeout(function () {
+            notification.classList.remove("show");
+        }, 2000);
+
+    });
+
+});
